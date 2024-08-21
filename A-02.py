08ca7ecaeaeea00312b31bb7813833786a72039f
Maira Tariq <mairatariq896@gmail.com>
2024-08-21 12:51:24 -0700

    add files

---
+++ A-02.py
@@ -0,0 +1,65 @@
+# Question 1 
+# //calculate age based on current year ?
+currentYear = 2024
+birthYear = 2003
+myAge = currentYear - birthYear
+print("myAgeis", myAge)
+# Question 2
+# //calculate the area of a rectangle using length and width variables ?
+length = 50
+width = 25
+areaofrectangle = length * width 
+print("AreaofRectangle",areaofrectangle)
+# Question 3
+# //calculate the area of a circle ?
+radius = 7
+pi = 3.1415
+areaofcircle =  pi * radius**2
+print("Areaofcircle", areaofcircle)
+# Question 4
+# //calculate the area of a cube ?
+a = 5
+surfaceareaofcube = 6 * a**2
+print("surfaceofcube",surfaceareaofcube)
+# Question 5
+# //create a program that convert a temperature from fahrenheit to celcius?
+fahrenheit = 77
+c = (fahrenheit-32) * 5/9
+print("fahrenheit to celcius",c)
+# Question 6
+# //convert a  given number of seconds into minutes and seconds using variables?
+# suppose we have 260 seconds
+seconds = 260
+minutes =  seconds / 60
+print("seconds to mintues",minutes)
+# Question 7
+# //write a program that calculates the percentage?
+number = 21
+percentagecalculate = number / 100
+print("percentagecalculate",percentagecalculate)
+# Question 7
+# //write a program that calculates the BMI using height in meters and weight in kilograms?
+weight = 70 
+height = 120
+BMI  = weight / height
+print("BMI",BMI)
+# Question 7
+# //write a program that calculates the volume of cylinder?
+pi = 3.14
+radius = 3
+height = 28
+volumeofcylinder = pi * radius**2 * height
+print("volumeofcylinder",volumeofcylinder)
+
+
+
+
+
+
+
+
+
+
+
+
+
